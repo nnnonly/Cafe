57248b21064d868365595e31a8c2e497de887d91
nnnonly <camnhunga4k49@gmail.com>
2020-06-04 15:46:19 +0700

    update

--- src/Interface/HomeDashBoards.java
+++ src/Interface/HomeDashBoards.java
@@ -8,8 +8,15 @@ import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.border.LineBorder;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
 import javax.swing.table.DefaultTableModel;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Font;
@@ -44,10 +51,10 @@ public class HomeDashBoards extends JFrame {
 	private JTextField tenSP;
 	private JTextField cost;
 	private JTextField textField_1;
-	private JTable table;
 	private JTable table_1;
 	private JTable table_2;
 	private JTable table_3;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -91,6 +98,12 @@ public class HomeDashBoards extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 285, 285);;
 		
+		bookTable();
+		menu();
+		listOrder();
+		
+	}
+	private void bookTable() {
 		JPanel panelTable = new JPanel();
 		panelTable.setBackground(new Color(204, 255, 153));
 		panelTable.setBounds(0, 46, 366, 706);
@@ -99,103 +112,77 @@ public class HomeDashBoards extends JFrame {
 		
 		JLabel lblTable = new JLabel("Book a Table");
 		lblTable.setFont(new Font("Tempus Sans ITC", Font.BOLD, 34));
-		lblTable.setBounds(93, 13, 224, 36);
+		lblTable.setBounds(91, 13, 224, 36);
 		panelTable.add(lblTable);
+		
+		JButton btnBook = new JButton("Book");
+		btnBook.setBackground(new Color(204, 255, 153));
+		btnBook.setBounds(257, 49, 97, 25);
+		panelTable.add(btnBook);
 
 		
 		//Table1
 		JPanel panel_table1 = new JPanel();
 		panel_table1.setBackground(new Color(204, 255, 153));
-		panel_table1.setBounds(56, 62, 103, 95);
+		panel_table1.setBounds(56, 76, 103, 95);
 		panelTable.add(panel_table1);
 		
 		JLabel table1 = new JLabel("");
 		panel_table1.add(table1);
-		table1.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table1.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table1.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table1.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 //		System.out.println(System.getProperty("user.dir"));
 		
 		JButton buttonTable1 = new JButton("Table 1");
 		buttonTable1.setBackground(new Color(204, 255, 153));
-		buttonTable1.setBounds(56, 154, 103, 20);
+		buttonTable1.setBounds(56, 170, 103, 20);
 		panelTable.add(buttonTable1);
 		
-		buttonTable1.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				buttonTable1.setBackground(new Color(238, 99, 99));
-				panel_table1.setBackground(new Color(238, 99, 99));
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
+
+		click(buttonTable1, panel_table1);
 		
 		//Table 2
 		JPanel panel_table2 = new JPanel();
 		panel_table2.setBackground(new Color(204, 255, 153));
-		panel_table2.setBounds(214, 62, 103, 95);
+		panel_table2.setBounds(212, 76, 103, 95);
 		panelTable.add(panel_table2);
 		
 		JLabel table2 = new JLabel("");
 		panel_table2.add(table2);
-		table2.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table2.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table2.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table2.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable2 = new JButton("Table 2");
 		buttonTable2.setBackground(new Color(204, 255, 153));
-		buttonTable2.setBounds(214, 154, 103, 20);
+		buttonTable2.setBounds(212, 170, 103, 20);
 		panelTable.add(buttonTable2);
 		
 		//Table3
 		JPanel panel_table3 = new JPanel();
 		panel_table3.setBackground(new Color(204, 255, 153));
-		panel_table3.setBounds(56, 187, 103, 95);
+		panel_table3.setBounds(56, 197, 103, 95);
 		panelTable.add(panel_table3);
 		
 		JLabel table3 = new JLabel("");
 		panel_table3.add(table3);
-		table3.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table3.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table3.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table3.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable3 = new JButton("Table 3");
 		buttonTable3.setBackground(new Color(204, 255, 153));
-		buttonTable3.setBounds(56, 280, 103, 20);
+		buttonTable3.setBounds(56, 293, 103, 20);
 		panelTable.add(buttonTable3);
 		
 		//Table 4
 		JPanel panel_table4 = new JPanel();
 		panel_table4.setBackground(new Color(204, 255, 153));
-		panel_table4.setBounds(214, 187, 103, 95);
+		panel_table4.setBounds(214, 197, 103, 95);
 		panelTable.add(panel_table4);
 		
 		JLabel table4 = new JLabel("");
 		panel_table4.add(table4);
-		table4.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table4.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table4.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table4.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable4 = new JButton("Table 4");
 //		buttonTable4.addActionListener(new ActionListener() {
@@ -203,126 +190,107 @@ public class HomeDashBoards extends JFrame {
 //			}
 //		});
 		buttonTable4.setBackground(new Color(204, 255, 153));
-		buttonTable4.setBounds(214, 280, 103, 20);
+		buttonTable4.setBounds(212, 293, 103, 20);
 		panelTable.add(buttonTable4);
 		
 		//Table 5
 		JPanel panel_table5 = new JPanel();
 		panel_table5.setBackground(new Color(204, 255, 153));
-		panel_table5.setBounds(56, 313, 103, 95);
+		panel_table5.setBounds(56, 326, 103, 95);
 		panelTable.add(panel_table5);
 		
 		JLabel table5 = new JLabel("");
 		panel_table5.add(table5);
-		table5.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table5.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table5.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table5.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable5 = new JButton("Table 5");
-		buttonTable5.setBackground(new Color(204, 255, 153));
-		buttonTable5.setBounds(56, 408, 103, 20);
+		buttonTable5.setBounds(56, 421, 103, 20);
 		panelTable.add(buttonTable5);
+		buttonTable5.setBackground(new Color(204, 255, 153));
 		
 		//Table 6
 		JPanel panel_table6 = new JPanel();
 		panel_table6.setBackground(new Color(204, 255, 153));
-		panel_table6.setBounds(214, 313, 103, 95);
+		panel_table6.setBounds(212, 326, 103, 95);
 		panelTable.add(panel_table6);
 		
 		JLabel table6 = new JLabel("");
 		panel_table6.add(table6);
-		table6.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table6.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table6.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table6.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable6 = new JButton("Table 6");
 		buttonTable6.setBackground(new Color(204, 255, 153));
-		buttonTable6.setBounds(214, 408, 103, 20);
+		buttonTable6.setBounds(214, 421, 103, 20);
 		panelTable.add(buttonTable6);
 		
 		//Table 7
 		JPanel panel_table7 = new JPanel();
 		panel_table7.setBackground(new Color(204, 255, 153));
-		panel_table7.setBounds(56, 441, 103, 95);
+		panel_table7.setBounds(56, 454, 103, 95);
 		panelTable.add(panel_table7);
 		
 		JLabel table7 = new JLabel("");
 		panel_table7.add(table7);
-		table7.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table7.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table7.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table7.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable7 = new JButton("Table 7");
 		buttonTable7.setBackground(new Color(204, 255, 153));
-		buttonTable7.setBounds(56, 536, 103, 20);
+		buttonTable7.setBounds(56, 548, 103, 20);
 		panelTable.add(buttonTable7);
 		
 		//Table 8
 		JPanel panel_table8 = new JPanel();
 		panel_table8.setBackground(new Color(204, 255, 153));
-		panel_table8.setBounds(214, 441, 103, 95);
+		panel_table8.setBounds(214, 454, 103, 95);
 		panelTable.add(panel_table8);
 		
 		JLabel table8 = new JLabel("");
 		panel_table8.add(table8);
-		table8.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table8.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table8.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table8.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable8 = new JButton("Table 8");
 		buttonTable8.setBackground(new Color(204, 255, 153));
-		buttonTable8.setBounds(214, 536, 103, 20);
+		buttonTable8.setBounds(214, 548, 103, 20);
 		panelTable.add(buttonTable8);
 		
 		//Table 9
 		JPanel panel_table9 = new JPanel();
 		panel_table9.setBackground(new Color(204, 255, 153));
-		panel_table9.setBounds(56, 569, 103, 95);
+		panel_table9.setBounds(56, 581, 103, 95);
 		panelTable.add(panel_table9);
 		
 		JLabel table9 = new JLabel("");
 		panel_table9.add(table9);
-		table9.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
-//		table9.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
+//		table9.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table9.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable9 = new JButton("Table 9");
 		buttonTable9.setBackground(new Color(204, 255, 153));
-		buttonTable9.setBounds(56, 660, 103, 20);
+		buttonTable9.setBounds(56, 673, 103, 20);
 		panelTable.add(buttonTable9);
 		
 		//Table 10
 		JPanel panel_table10 = new JPanel();
 		panel_table10.setBackground(new Color(204, 255, 153));
-		panel_table10.setBounds(214, 569, 103, 95);
+		panel_table10.setBounds(214, 581, 103, 95);
 		panelTable.add(panel_table10);
 		
 		JLabel table10 = new JLabel("");
 		panel_table10.add(table10);
-		table10.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+//		table10.setIcon(new ImageIcon("C:\\Users\\DEll\\Desktop\\cafe_96px.png"));
+		table10.setIcon(new ImageIcon(getCurrentStaticContext("cafe_96px.png")));
 		
 		JButton buttonTable10 = new JButton("Table 10");
 		buttonTable10.setBackground(new Color(204, 255, 153));
-		buttonTable10.setBounds(214, 660, 103, 20);
+		buttonTable10.setBounds(214, 673, 103, 20);
 		panelTable.add(buttonTable10);
-		
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(255, 255, 255));
-		panel_1.setBounds(0, 0, 1258, 47);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel label = new JLabel("-");
-		label.setForeground(new Color(51, 153, 153));
-		label.setFont(new Font("Showcard Gothic", Font.BOLD, 32));
-		label.setBounds(1137, 0, 39, 47);
-		panel_1.add(label);
-		
-		JButton btnX = new JButton("X");
-		btnX.setForeground(new Color(51, 153, 153));
-		btnX.setFont(new Font("Showcard Gothic", Font.BOLD, 31));
-		btnX.setBounds(1188, 0, 70, 47);
-		btnX.setBackground(new Color(255, 255, 255));
-		panel_1.add(btnX);
-		
-		
-		
+	}
+	
+	private void menu() {
 		JPanel pnMenu = new JPanel();
 		pnMenu.setBackground(new Color(204, 255, 153));
 		pnMenu.setBounds(421, 46, 384, 706);
@@ -340,8 +308,10 @@ public class HomeDashBoards extends JFrame {
 		tab2.setLayout(null);
 		
 		textField_1 = new JTextField();
+		textField_1.setEditable(false);
 		textField_1.setColumns(10);
 		textField_1.setBounds(12, 27, 203, 30);
+		textField_1.setBackground(new Color(255, 255, 255));
 		tab2.add(textField_1);
 		
 		JButton button_1 = new JButton("Search");
@@ -355,7 +325,7 @@ public class HomeDashBoards extends JFrame {
 		panel_2.setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 0, 368, 326);
+		scrollPane.setBounds(0, 0, 356, 326);
 		panel_2.add(scrollPane);
 		
 		table_1 = new JTable();
@@ -392,7 +362,9 @@ public class HomeDashBoards extends JFrame {
 		tab1.setLayout(null);
 		
 		textField_4 = new JTextField();
+		textField_4.setEditable(false);
 		textField_4.setBounds(12, 25, 203, 30);
+		textField_4.setBackground(new Color(255, 255, 255));
 		tab1.add(textField_4);
 		textField_4.setColumns(10);
 		
@@ -411,6 +383,7 @@ public class HomeDashBoards extends JFrame {
 		panel_3.add(scrollPane_1);
 		
 		table_2 = new JTable();
+		table_2.setEnabled(false);
 		table_2.setModel(new DefaultTableModel(
 			new Object[][] {
 				{null, null, null},
@@ -447,16 +420,42 @@ public class HomeDashBoards extends JFrame {
 		pnMenu.add(lblDescription);
 		
 		tenSP = new JTextField();
+		tenSP.setEditable(false);
+		tenSP.setBackground(new Color(255, 255, 255));
 		tenSP.setBounds(22, 598, 143, 28);
 		pnMenu.add(tenSP);
 		tenSP.setColumns(10);
 		
 		cost = new JTextField();
+		cost.setEditable(false);
+		cost.setBackground(new Color(255, 255, 255));
 		cost.setColumns(10);
 		cost.setBounds(22, 638, 143, 28);
 		pnMenu.add(cost);
+	}
+	
+	private void header() {
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 255));
+		panel_1.setBounds(0, 0, 1258, 47);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
 		
+		JLabel label = new JLabel("-");
+		label.setForeground(new Color(51, 153, 153));
+		label.setFont(new Font("Showcard Gothic", Font.BOLD, 32));
+		label.setBounds(1137, 0, 39, 47);
+		panel_1.add(label);
 		
+		JButton btnX = new JButton("X");
+		btnX.setForeground(new Color(51, 153, 153));
+		btnX.setFont(new Font("Showcard Gothic", Font.BOLD, 31));
+		btnX.setBounds(1188, 0, 70, 47);
+		btnX.setBackground(new Color(255, 255, 255));
+		panel_1.add(btnX);
+	}
+	
+	private void listOrder() {
 		JPanel pnListOrder = new JPanel();
 		pnListOrder.setBackground(new Color(204, 255, 153));
 		pnListOrder.setBounds(837, 46, 421, 706);
@@ -469,7 +468,9 @@ public class HomeDashBoards extends JFrame {
 		pnListOrder.add(lblIdBill);
 		
 		textField = new JTextField();
+		textField.setEditable(false);
 		textField.setBounds(75, 13, 124, 29);
+		textField.setBackground(new Color(255, 255, 255));
 		pnListOrder.add(textField);
 		textField.setColumns(10);
 		
@@ -479,8 +480,10 @@ public class HomeDashBoards extends JFrame {
 		pnListOrder.add(lblTable_1);
 		
 		textTable = new JTextField();
+		textTable.setEditable(false);
 		textTable.setColumns(10);
 		textTable.setBounds(279, 13, 110, 29);
+		textTable.setBackground(new Color(255, 255, 255));
 		pnListOrder.add(textTable);
 		
 		JLabel label_1 = new JLabel(" Date");
@@ -489,8 +492,10 @@ public class HomeDashBoards extends JFrame {
 		pnListOrder.add(label_1);
 		
 		textField_2 = new JTextField();
+		textField_2.setEditable(false);
 		textField_2.setColumns(10);
 		textField_2.setBounds(75, 62, 124, 29);
+		textField_2.setBackground(new Color(255, 255, 255));
 		pnListOrder.add(textField_2);
 		
 		JLabel label_2 = new JLabel("Time");
@@ -499,8 +504,10 @@ public class HomeDashBoards extends JFrame {
 		pnListOrder.add(label_2);
 		
 		textField_3 = new JTextField();
+		textField_3.setEditable(false);
 		textField_3.setColumns(10);
 		textField_3.setBounds(279, 62, 110, 29);
+		textField_3.setBackground(new Color(255, 255, 255));
 		pnListOrder.add(textField_3);
 		
 		JLabel label_3 = new JLabel(" List Order");
@@ -526,7 +533,7 @@ public class HomeDashBoards extends JFrame {
 		panel_4.setLayout(null);
 		
 		JScrollPane scrollPane_2 = new JScrollPane();
-		scrollPane_2.setBounds(0, 0, 421, 338);
+		scrollPane_2.setBounds(0, 0, 421, 190);
 		panel_4.add(scrollPane_2);
 		
 		table_3 = new JTable();
@@ -549,10 +556,60 @@ public class HomeDashBoards extends JFrame {
 		));
 		scrollPane_2.setViewportView(table_3);
 		
-		table = new JTable();
-		table.setBounds(421, 377, 1, 1);
-		contentPane.add(table);
+		JLabel lblTotal = new JLabel("Total:");
+		lblTotal.setFont(new Font("SimSun", Font.PLAIN, 21));
+		lblTotal.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTotal.setBounds(81, 245, 93, 29);
+		panel_4.add(lblTotal);
 		
+		textField_5 = new JTextField();
+		textField_5.setBackground(new Color(255, 255, 255));
+		textField_5.setEditable(false);
+		textField_5.setBounds(174, 244, 155, 36);
+		panel_4.add(textField_5);
+		textField_5.setColumns(10);
 		
+		JLabel lblVnd = new JLabel("VND");
+		lblVnd.setFont(new Font("SimSun", Font.PLAIN, 21));
+		lblVnd.setBounds(341, 251, 56, 16);
+		panel_4.add(lblVnd);
+	}
+	
+	 private void click(JButton button, JPanel panelButton) {
+		 button.addMouseListener(new MouseListener() {
+				
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					// TODO Auto-generated method stub
+//					buttonTable1.setBackground(new Color(238, 99, 99));
+//					panel_table1.setBackground(new Color(238, 99, 99));
+				}
+				
+				@Override
+				public void mousePressed(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					button.setBackground(new Color(238, 99, 99));
+					panelButton.setBackground(new Color(238, 99, 99));
+				}
+				
+				@Override
+				public void mouseExited(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+				}
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+					button.setBackground(new Color(238, 99, 99));
+					panelButton.setBackground(new Color(238, 99, 99));
+					
+				}
+
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					// TODO Auto-generated method stub
+					button.setBackground(new Color(204, 255, 153));
+					panelButton.setBackground(new Color(204, 255, 153));
+				}
+			});
 	}
 }
